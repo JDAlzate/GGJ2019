5bc6936823417f6bdbc8d9a95f8c7c444adaae4b
Juan Alzate <juandiegoalzatemejia@gmail.com>
2019-01-26 13:23:56 -0500

    LeverScript

---
+++ GGJ2019/Assets/Scripts/LeverScript.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeverScript : MonoBehaviour {
+
+    bool canPull;
+
+    Transform platform;
+
+    Vector3 initialPosition;
+
+    Transform targetTransform;
+
+    bool isTowardsTarget;
+
+	// Use this for initialization
+	void Start ()
+    {
+        platform = transform.parent;
+        targetTransform = platform.Find("Target").transform;
+        targetTransform.parent = null;
+        transform.parent = null;
+        initialPosition = platform.position;
+
+        canPull = false;
+        isTowardsTarget = false;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetMouseButtonDown(1) && canPull)
+        {
+            isTowardsTarget = !isTowardsTarget;
+        }
+
+        if (!isTowardsTarget)
+            GoTowards(initialPosition);
+        else
+            GoTowards(targetTransform.position);
+    }
+
+    private void GoTowards(Vector3 targetPosition)
+    {
+        if (Vector3.Distance(platform.transform.position, targetPosition) > 0.1f)
+        {
+            platform.transform.position = Vector3.Lerp(platform.transform.position, targetPosition, 0.05f);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            canPull = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            canPull = false;
+        }
+    }
+}
