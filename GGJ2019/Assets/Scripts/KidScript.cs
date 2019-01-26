64a8d2d566efd8ac2f021bb251a6a428b075188d
Daniel Ortuno Murguey <DEOrtunoMurguey@student.fullsail.edu>
2019-01-25 20:06:04 -0500

    Assets downloaded

--- GGJ2019/Assets/Scripts/KidScript.cs
+++ GGJ2019/Assets/Scripts/KidScript.cs
@@ -6,18 +6,37 @@ public class KidScript : MonoBehaviour {
 
     [SerializeField] float movementSpeed;
 
-    Rigidbody kidRb;
-    Rigidbody dogRb;
+    Rigidbody rb;
+    bool isJumping;
 
 	// Use this for initialization
 	void Start ()
     {
-		
+        rb = GetComponent<Rigidbody>();
+        isJumping = false;
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-		
+        PlayerMovement();
 	}
+
+    void PlayerMovement()
+    {
+        rb.velocity = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Horizontal")) * movementSpeed * Time.deltaTime;
+
+        if(Input.GetKeyDown(KeyCode.Space) && !isJumping)
+        {
+
+        }
+    }
+
+
+    IEnumerator JumpCoroutine()
+    {
+
+        yield return null;
+    }
+
 }
