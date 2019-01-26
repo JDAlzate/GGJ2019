2b5acc61f821f49f4df3e908a100ecad8444b091
Daniel Ortuno Murguey <DEOrtunoMurguey@student.fullsail.edu>
2019-01-25 21:35:39 -0500

    Kid movement and jump

--- GGJ2019/Assets/Scripts/KidScript.cs
+++ GGJ2019/Assets/Scripts/KidScript.cs
@@ -5,14 +5,17 @@ using UnityEngine;
 public class KidScript : MonoBehaviour {
 
     [SerializeField] float movementSpeed;
+    [SerializeField] float jumpSpeed;
 
     Rigidbody rb;
+    Animator animator;
     bool isJumping;
 
 	// Use this for initialization
 	void Start ()
     {
         rb = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
         isJumping = false;
 	}
 	
@@ -24,19 +27,62 @@ public class KidScript : MonoBehaviour {
 
     void PlayerMovement()
     {
-        rb.velocity = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Horizontal")) * movementSpeed * Time.deltaTime;
+        rb.velocity = new Vector2(Input.GetAxis("Horizontal") * movementSpeed * Time.deltaTime, rb.velocity.y);
+        animator.SetFloat("Speed", Mathf.Abs(rb.velocity.x));
+
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            transform.eulerAngles = new Vector3(transform.eulerAngles.x, 90, transform.eulerAngles.z);
+        }
+        
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            transform.eulerAngles = new Vector3(transform.eulerAngles.x, -90, transform.eulerAngles.z);
+        }
 
         if(Input.GetKeyDown(KeyCode.Space) && !isJumping)
         {
-
+            StartCoroutine(JumpCoroutine());
         }
     }
 
 
     IEnumerator JumpCoroutine()
     {
+        isJumping = true;
 
-        yield return null;
+        rb.velocity = new Vector2(rb.velocity.x, 0);
+        //Add force on the first frame of the jump
+        rb.AddForce(Vector2.up * 8, ForceMode.Impulse);
+
+        float jumpTime = 2;
+        float currentTime = 0;
+
+        while(currentTime < jumpTime)
+        {
+            currentTime += Time.deltaTime;
+
+            float verticalVel = Mathf.Lerp(jumpSpeed, 0, currentTime / jumpTime);
+            rb.velocity = new Vector2(rb.velocity.x, verticalVel);
+
+            yield return null;
+        }
+
+        rb.velocity = new Vector2(rb.velocity.x, -2);
+
+        yield return new WaitUntil(() => !isJumping);
+
+        rb.velocity = new Vector2(rb.velocity.x, 0);
     }
 
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.transform.tag == "Ground")
+        {
+            if (isJumping)
+                isJumping = false;
+        }
+    }
 }
