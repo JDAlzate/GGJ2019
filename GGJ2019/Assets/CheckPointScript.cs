027b870cb7d8581a74c0dc2050695ea6da6adc4a
Juan Alzate <juandiegoalzatemejia@gmail.com>
2019-01-26 18:25:29 -0500

    Added chekcpointScript

---
+++ GGJ2019/Assets/CheckPointScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPointScript : MonoBehaviour {
+
+    bool isTriggered;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player") && !isTriggered)
+        {
+            
+        }
+    }
+}
